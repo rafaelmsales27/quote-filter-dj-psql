deb5737f45024358a88e42135ace8ec592c0299b
Rafael Sales <rafaelmsales27@gmail.com>
2024-05-19 03:54:54 +0200

    setup basic views

--- filter/urls.py
+++ filter/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="filter"),
+    path("form/", views.form, name="form"),
+    path("<int:question_id>/", views.detail, name="detail"),
 ]
--- filter/views.py
+++ filter/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +7,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("You are at filter")
+
+
+def form(request):
+    active_questions = Question.objects.filter(active=True).order_by("order")
+    context = {"questions": active_questions}
+    return render(request, "filter/form.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "filter/detail.html", {"question": question})
